e0aa9289b12e9c65d2940bfdaee1204109679be1
iAlexander <alexander@iashchuk.com>
2016-11-07 00:13:11 +0200

    Finalized the prototype of the application

--- plantsensor/ViewController.swift
+++ plantsensor/ViewController.swift
@@ -7,21 +7,29 @@
 //
 
 import UIKit
+import Foundation
 import NotificationCenter
+import ImageIO
 
 class ViewController: UIViewController {
     
     @IBOutlet weak var brightnessTextLabel: UILabel!
     @IBOutlet weak var brightnessManualTextLabel: UILabel!
+    @IBOutlet weak var loadActivityIndicator: UIActivityIndicatorView!
     @IBOutlet weak var brightnessProgressView: UIProgressView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         NotificationCenter.default.addObserver(self, selector: #selector(self.UIScreenBrightnessDidChange), name: Notification.Name.UIScreenBrightnessDidChange, object: nil)
-        NotificationCenter.default.post(name: Notification.Name.UIScreenBrightnessDidChange, object: nil)
+        loadActivityIndicator.startAnimating()
+        let delayInSeconds = 4.0
+        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + delayInSeconds) {
+            self.loadActivityIndicator.stopAnimating()
+            NotificationCenter.default.post(name: Notification.Name.UIScreenBrightnessDidChange, object: nil)
+        }
     }
     
-    @objc private func UIScreenBrightnessDidChange(notification: NSNotification) {
+    func UIScreenBrightnessDidChange(notification: NSNotification) {
         let brightnessFloatValue = Float(UIScreen.main.brightness)
         let brightnessPercentValue = brightnessFloatValue * 100
         brightnessTextLabel.text = String(format: "Brightness level: %.1f", brightnessPercentValue) + "%"
